93636ddbdb241382bb46b8c36d1201570112b0a6
lleehh <haland@barbermesteren.no>
2025-01-24 19:04:49 +0100

    added components

---
+++ components/equipment/equipment-component.tsx
@@ -0,0 +1,28 @@
+'use client'
+import {IdentifiedObject} from "@/models/cim";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
+interface EquipmentProps {
+    equipment: IdentifiedObject
+}
+
+export default function EquipmentComponent({equipment}: EquipmentProps) {
+    return (
+        <Card className="w-[350px]">
+            <CardHeader>
+                <CardTitle>{equipment.rdfType}</CardTitle>
+                <CardDescription>{equipment.name}</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <div className="text-gray-400">{equipment.description}</div>
+            </CardContent>
+        </Card>
+    )
+}
