83cc0781bebb06a2c499ddffd4466dadfe991d61
Henrik Olai Olsbø Håland <156893021+HenrikHaaland@users.noreply.github.com>
2025-01-29 14:13:24 +0100

    added custom flow node

--- components/dig/flow.tsx
+++ components/dig/flow.tsx
@@ -3,21 +3,24 @@ import {
     ReactFlow,
     useNodesState,
     useEdgesState,
-    addEdge, Viewport, Controls, MiniMap, Background, Panel
+    addEdge, Viewport, Controls, MiniMap, Background, Panel, Node
 } from '@xyflow/react';
 
 import '@xyflow/react/dist/style.css';
 import {useCallback} from "react";
 import {Button} from "@/components/ui/button";
+import BreakerComponent, { FlowBreakerComponent } from '../equipment/breaker';
+import { Breaker } from '@/models/cim';
+
+export type BreakerNode = Node<Breaker, 'breaker'>;
 
 const initialNodes = [
     {id: '1', position: {x: 0, y: 0}, data: {label: '1'}},
-    {id: '2', position: {x: 0, y: 100}, data: {label: '2'}},
+    {id: '2', position: {x: 0, y: 100}, data: {label: '2'}, type: 'breaker'},
 ];
 const initialEdges = [{id: 'e1-2', source: '1', target: '2'}];
 
 export default function Dig() {
-
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [nodes, _, onNodesChange] = useNodesState(initialNodes);
@@ -35,6 +38,8 @@ export default function Dig() {
 
     const defaultViewport: Viewport = {x: 100, y: 300, zoom: 0.8};
 
+    const nodeTypes = { breaker: FlowBreakerComponent};
+
     return (
         <div className="relative flex flex-col h-full flex-grow">
             <ReactFlow style={{width: '100%', height: '100%'}}
@@ -44,6 +49,7 @@ export default function Dig() {
                        onEdgesChange={onEdgesChange}
                        onConnect={onConnect}
                        defaultViewport={defaultViewport}
+                       nodeTypes={nodeTypes}
             >
                 <Panel position="top-right">
                     <div
--- components/equipment/breaker.tsx
+++ components/equipment/breaker.tsx
@@ -9,38 +9,61 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import {ComponentIcon} from "@/components/component-icon";
+import type { Node, NodeProps, XYPosition } from '@xyflow/react';
+import {Handle, Position} from "@xyflow/react";
+import { BreakerNode } from "@/components/dig/flow";
 
 
 interface BreakerProps {
-    equipment: Breaker
+    equipment?: Breaker
     displayName: string
 }
 
+export interface BreakerFlowProps {
+    id: string
+    position: XYPosition
+    data: Record<string, BreakerProps | null>
 
-export default function BreakerComponent({equipment, displayName}: BreakerProps) {
 
+}
+         
+    export default function BreakerComponent({equipment, displayName}: BreakerProps) {
+
+    
     return (
         <Card className="w-[350px]">
             <CardHeader>
                 <CardTitle>
                     <div className="flex flex-row items-center gap-2">
                         <ComponentIcon icon="bryter"/>
-                        {equipment.rdfType}
+                        {/* {equipment.rdfType} */}
                     </div>
                 </CardTitle>
-                <CardDescription>{equipment.name}</CardDescription>
+                {/* <CardDescription>{equipment.name}</CardDescription> */}
             </CardHeader>
             <CardContent>
-                <div className="text-gray-400">{equipment.description}</div>
+                {/* <div className="text-gray-400">{equipment.description}</div> */}
             </CardContent>
             <CardFooter className="text-green-300 rounded-md">
                 {displayName}
             </CardFooter>
             <CardContent>
                 <div>
-                    Voltage {(equipment.baseVoltage as BaseVoltage).name}
+                    {/* Voltage {(equipment.baseVoltage as BaseVoltage).name} */}
                 </div>
             </CardContent>
         </Card>
+
+    )
+}
+
+
+export function FlowBreakerComponent({data}: NodeProps<BreakerNode>) {
+    return (
+        <div>
+            <Handle type="target" position={Position.Left} className="!w-3 !h-3 !rounded-none !bg-stone-400"/>
+            <BreakerComponent displayName="Breaker"/>
+            <Handle type="source" position={Position.Right} className="!w-3 !h-3 !rounded-none !bg-stone-400" id=""/>
+        </div>
     )
 }
