d5fd3f8f96d17e966171585218273ef953e955fd
ivbaa003 <ivbaa003@osloskolen.no>
2025-02-07 10:13:31 +0100

    Fikset at 2 terminaler automatisk åpner når du starter.

--- components/dig/placeholder.tsx
+++ components/dig/placeholder.tsx
@@ -4,7 +4,7 @@ export default function Placeholder () {
     return (
 
     <div className="w-44 border border-gray-400 p-3">
-        <div role="form" className="w-full mb-1 bg-gray-200 dark:bg-gray-700 flex-grow h-4"/>
+        <div role="form" className="w-full mb-1 bg-red-500 dark:bg-gray-700 flex-grow h-4"/>
         <div role="form" className="w-full mb-1 bg-gray-200 dark:bg-gray-700 flex-grow h-4"/>
         <div role="form" className="w-full mb-1 bg-gray-200 dark:bg-gray-700 flex-grow h-4"/>
     </div> 
