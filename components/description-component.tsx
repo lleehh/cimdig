6479086f8e84444c57d2b10c4bf2f560d707ea8e
matszook <mats.h.haartveit@gmail.com>
2026-02-02 15:10:31 +0100

    fix(descprition): use asChild
    The button got an error when not using asChild

--- components/description-component.tsx
+++ components/description-component.tsx
@@ -1,12 +1,12 @@
 import { DropdownMenu, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
-import { Button } from "./ui/button";
+import {Button} from "@/components/ui/button";
 import { MessageSquareText } from "lucide-react";
 
 export default function Description() {
 
     return(
         <DropdownMenu>
-            <DropdownMenuTrigger>
+            <DropdownMenuTrigger asChild={true}>
                 <Button variant="ghost" size="icon"><MessageSquareText/> </Button>
             </DropdownMenuTrigger>
         </DropdownMenu>
