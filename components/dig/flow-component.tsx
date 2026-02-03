ac49cf2cfe7b09cda7fb6acbffd947d53af5531f
Oscar Vagle <oscar.vagle@statnett.no>
2026-02-03 09:41:09 +0100

    refactor(flow-component): remove logging

--- components/dig/flow-component.tsx
+++ components/dig/flow-component.tsx
@@ -56,8 +56,6 @@ export default function FlowComponent({data}: NodeProps<CimNode>) {
     }, []);
 
     const handleExpand = async () => {
-        console.log("All Nodes", nodes)
-        console.log("All Edges", edges)
         // We need to load the full component from the database to get all the properties
 
         const node = nodes.find(node => node.id === component?.rdfId)
@@ -99,7 +97,6 @@ export default function FlowComponent({data}: NodeProps<CimNode>) {
                 let terminals = component.terminals || []
                 if(terminals.length == 0 && (component as PowerTransformerEnd).terminal != undefined)
                     terminals = [(component as PowerTransformerEnd).terminal]
-                console.log("Terminals:", terminals)
                 terminals.forEach(terminal => {
                     if (!doesEquipmentExistsInFlow(terminal.rdfId, nodes)) {
                         newNodes.push(createNode(terminal.rdfId, terminal, 0, 0, data.otherData.color))
@@ -120,8 +117,6 @@ export default function FlowComponent({data}: NodeProps<CimNode>) {
             setEdges([...edges, ...newEdges])
             setFocusNode(newNodes[newNodes.length - 1].id)
         }
-        console.log("New Nodes:", newNodes)
-        console.log("New Edges:", newEdges)
         setExpanded(true)
     }
 
