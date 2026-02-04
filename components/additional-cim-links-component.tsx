e17b6fc481fe86dd4b9845266e384b2af6cbd3de
Oscar Vagle <oscar.vagle@yahoo.no>
2026-02-04 12:50:58 +0100

    80 add info button to react flow components (#117)
    * feat(description): Create description button
    * docs(flow-util): added more JSDoc docstrings (and to cim-service)
    
    * fix(description): use asChild
    
    The button got an error when not using asChild
    
    * Feat(description): Actually added a description
    
    * fix(ui): improve dropdown interaction for component buttons
    
    * docs(flow-util): added JS doc comments for...
    
    flow-util and cim-service methods
    
    * docs(flow-util): update docstring
    
    * Update btn-group-component.tsx
    
    * Update btn-group-component.tsx
    
    ---------
    
    Co-authored-by: matszook <mats.h.haartveit@gmail.com>

--- components/additional-cim-links-component.tsx
+++ components/additional-cim-links-component.tsx
@@ -1,83 +1,80 @@
 'use client'
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel, DropdownMenuPortal,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-import {List} from "lucide-react";
-import {CIM, IdentifiedObject} from "@/lib/cim";
-import useFlowStore, {selector} from "@/lib/store/store-flow";
-import {useShallow} from "zustand/react/shallow";
-import {getComponentById} from "@/lib/store/model-repository";
-import {ComponentStatus, componentStatus, createEdge, createNode} from "@/lib/flow-utils";
-import {Button} from "@/components/ui/button";
+import { List } from "lucide-react";
+import { CIM, IdentifiedObject } from "@/lib/cim";
+import useFlowStore, { selector } from "@/lib/store/store-flow";
+import { useShallow } from "zustand/react/shallow";
+import { getComponentById } from "@/lib/store/model-repository";
+import { ComponentStatus, createEdge, createNode } from "@/lib/flow-utils";
+import { Button } from "@/components/ui/button";
 
 interface CimLinksProps {
-    component: CIM
-    componentRefs: ComponentStatus[]
+  component: CIM
+  componentRefs: ComponentStatus[]
+  open: boolean
+  onOpenChange: (open: boolean) => void
 }
 
-const AdditionalCimLinks = ({component, componentRefs}: CimLinksProps) => {
+const AdditionalCimLinks = ({ component, componentRefs, open, onOpenChange }: CimLinksProps) => {
+  const { nodes, edges, setNodes, setEdges, setFocusNode } = useFlowStore(useShallow(selector));
 
-    const {
-        nodes,
-        edges,
-        setNodes,
-        setEdges,
-        setFocusNode
-    } = useFlowStore(useShallow(selector));
+  const handleSelect = async (id: string) => {
+    const refComponent = await getComponentById(id)
 
-
-    const handleSelect = async (id: string) => {
-
-        const refComponent = await getComponentById(id)
-
-        if (refComponent != null) {
-            if (nodes.find(node => node.data.rdfId === refComponent.rdfId) === undefined) {
-                const newNode = createNode(refComponent.rdfId, refComponent, 0, 0)
-                const newEdge = createEdge(component.rdfId, refComponent.rdfId, true, "topHandle", "bottomHandle")
-                setNodes([...nodes, newNode])
-                setEdges([...edges, newEdge])
-                setFocusNode(newNode.id)
-            } else {
-                console.log("COnnect to existsing ")
-                const newEdge = createEdge(component.rdfId, refComponent.rdfId, true, "topHandle", "bottomHandle")
-                setEdges([...edges, newEdge])
-            }
-        }
-
+    if (refComponent != null) {
+      if (nodes.find(node => node.data.rdfId === refComponent.rdfId) === undefined) {
+        const newNode = createNode(refComponent.rdfId, refComponent, 0, 0)
+        const newEdge = createEdge(component.rdfId, refComponent.rdfId, true, "topHandle", "bottomHandle")
+        setNodes([...nodes, newNode])
+        setEdges([...edges, newEdge])
+        setFocusNode(newNode.id)
+      } else {
+        const newEdge = createEdge(component.rdfId, refComponent.rdfId, true, "topHandle", "bottomHandle")
+        setEdges([...edges, newEdge])
+      }
     }
 
-    const filteredRefs = componentRefs.filter(status => (status.exists == true && status.connected === false) || status.exists == false)
+    // optional: close after selecting
+    onOpenChange(false);
+  }
 
+  const filteredRefs = componentRefs.filter(
+    status => (status.exists === true && status.connected === false) || status.exists === false
+  )
 
-    return (
-        <DropdownMenu modal={false}>
-            <DropdownMenuTrigger asChild={true}>
-                <Button variant="ghost" size="icon">
-                    <List/>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuPortal>
-                <DropdownMenuContent className="flex flex-col space-y-2 max-h-64 overflow-y-auto">
-                    <DropdownMenuLabel>Links to other components</DropdownMenuLabel>
-                    <DropdownMenuSeparator/>
-                    <>
-                        {filteredRefs.map((component) => (
-                            <DropdownMenuItem key={component.equipment.rdfId}
-                                              onSelect={() => handleSelect(component.equipment.rdfId)}>
-                                {component.equipment.rdfType} {(component.equipment as IdentifiedObject)?.name}
-                                {component.exists && !component.connected ? "(Create new Link)" : ""}
-                            </DropdownMenuItem>
-                        ))}
-                    </>
-                </DropdownMenuContent>
-            </DropdownMenuPortal>
-        </DropdownMenu>
-    )
+  return (
+    <DropdownMenu open={open} onOpenChange={onOpenChange} modal={false}>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <List />
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuPortal>
+        <DropdownMenuContent className="flex flex-col space-y-2 max-h-64 overflow-y-auto">
+          <DropdownMenuLabel>Links to other components</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {filteredRefs.map((c) => (
+            <DropdownMenuItem
+              key={c.equipment.rdfId}
+              onSelect={() => handleSelect(c.equipment.rdfId)}
+            >
+              {c.equipment.rdfType} {(c.equipment as IdentifiedObject)?.name}
+              {c.exists && !c.connected ? " (Create new Link)" : ""}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenuPortal>
+    </DropdownMenu>
+  )
 }
 
 export default AdditionalCimLinks;
