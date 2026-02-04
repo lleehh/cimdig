5b6316ce94a68c8fc4c1456fcf281aa1b51d7c61
Iver Morland Karlsvik <iver.karlsvik@statnett.no>
2026-02-04 11:07:18 +0100

    chore: remove bay-component from dig/page

--- app/dig/page.tsx
+++ app/dig/page.tsx
@@ -12,7 +12,6 @@ import {
 } from "@/components/ui/breadcrumb";
 import Dig from "@/components/dig/flow";
 import FlowRoot from "@/components/dig/flow_root";
-import Baycomponent from "@/components/equipment/bay-component";
 import { Sub } from "@radix-ui/react-dropdown-menu";
 import { Power } from "lucide-react";
 
