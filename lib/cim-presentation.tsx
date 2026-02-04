342d0d08d4aafccdf7974859a95baa6976edfd14
Iver Morland Karlsvik <119415554+ivermoka@users.noreply.github.com>
2026-02-04 13:26:26 +0100

    feat: generic cim component (#121)
    * feat(generic): added back changes from other branch
    * feat(generic-component): Added generic component to be used for CIM components.
    
    * fix(cim-component): use new args
    
    * feat(genericComponet): added cardContent
    
    * fix: toString the description
    
    * fix: use correct component for page and cim-components
    
    * fix: correct sizeing on components in page.tsx
    
    * fix(gallery): cn small
    
    * fix(generic): more functionality to make it closer to prev
    
    * refactor: streamlined generic-component usage
    
    * fix(lint): removed whitespaces
    
    * fix(generic): more in-depth title handling
    
    * refactor: remove old map
    
    * fix(style): truncate text
    
    * fix(style): truncate text in title + icon not shrink
    
    * chore: remove deprecated components import
    
    * chore: removed unused components
    
    * chore: remove bay-component from dig/page
    
    * refactor(generic-component): made truncate text not hardcoded, but rather a const
    
    * refactor(utils.ts): made cast more specific
    
    * refactor(generic-component): removed tailwind class overflow-hidden
    
    ---------
    
    Co-authored-by: Iver Morland Karlsvik <iver.karlsvik@statnett.no>
    Co-authored-by: Nicolas Ovidian Winsnes Marti <nicolas@Nicolass-MacBook-Pro.local>

---
+++ lib/cim-presentation.tsx
@@ -0,0 +1,80 @@
+import { ReactElement } from "react"
+import {
+	Circle,
+	Factory,
+	HousePlug,
+	LandPlot,
+	Shell,
+	SquareTerminal,
+	Triangle
+} from "lucide-react"
+import { mediumComponentStyling, smallComponentStyling } from "../components/dig/flow-component"
+import { ComponentIcon } from "../components/component-icon";
+
+export interface CimPresentation {
+	size: () => string
+	icon: ReactElement
+}
+
+export const cimPresentationMap: Record<string, CimPresentation> = {
+	"cim:ACLineSegment": {
+		size: mediumComponentStyling,
+		icon: <ComponentIcon icon="ledningssegment" />
+	},
+	"cim:Terminal": {
+		size: smallComponentStyling,
+		icon: <SquareTerminal />
+	},
+	"cim:ConnectivityNode": {
+		size: smallComponentStyling,
+		icon: <Shell />
+	},
+	"cim:Breaker": {
+		size: mediumComponentStyling,
+		icon: <ComponentIcon icon="bryter" />
+	},
+	"cim:GeneratingUnit": {
+		size: mediumComponentStyling,
+		icon: <ComponentIcon icon="generator" />
+	},
+	"cim:NonConformLoad": {
+		size: mediumComponentStyling,
+		icon: <Factory />
+	},
+	"cim:BusbarSection": {
+		size: mediumComponentStyling,
+		icon: <ComponentIcon icon="samleskinne" />
+	},
+	"cim:Bay": {
+		size: mediumComponentStyling,
+		icon: <LandPlot />
+
+	},
+	"cim:Substation": {
+		size: mediumComponentStyling,
+		icon: <ComponentIcon icon="stasjon" />
+	},
+	"cim:PowerTransformer": {
+		size: smallComponentStyling,
+		icon: <ComponentIcon icon="transformator" />
+
+	},
+	"cim:PowerTransformerEnd": {
+		size: smallComponentStyling,
+		icon: <Circle />
+	},
+	"cim:Line": {
+		size: smallComponentStyling,
+		icon: <ComponentIcon icon="overforing" />
+	},
+
+	"cim:ConformLoad": {
+		size: mediumComponentStyling,
+		icon: <HousePlug />
+	}
+}
+
+export const defaultCimPresentation: CimPresentation = {
+	size: mediumComponentStyling,
+	icon: <Triangle />
+}
