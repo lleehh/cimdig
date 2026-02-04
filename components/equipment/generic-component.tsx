c6166fdca5509329ade2a51a7601fca18533b1f5
Iver Morland Karlsvik <119415554+ivermoka@users.noreply.github.com>
2026-02-04 13:26:26 +0100

    feat: generic cim component (#121)
    * feat(generic): added back changes from other branch
    * feat(generic-component): Added generic component to be used for CIM components.
    
    * fix(cim-component): use new args
    
    * feat(genericComponet): added cardContent
    
    * fix: toString the description
    
    * fix: use correct component for page and cim-components
    
    * fix: correct sizeing on components in page.tsx
    
    * fix(gallery): cn small
    
    * fix(generic): more functionality to make it closer to prev
    
    * refactor: streamlined generic-component usage
    
    * fix(lint): removed whitespaces
    
    * fix(generic): more in-depth title handling
    
    * refactor: remove old map
    
    * fix(style): truncate text
    
    * fix(style): truncate text in title + icon not shrink
    
    * chore: remove deprecated components import
    
    * chore: removed unused components
    
    * chore: remove bay-component from dig/page
    
    * refactor(generic-component): made truncate text not hardcoded, but rather a const
    
    * refactor(utils.ts): made cast more specific
    
    * refactor(generic-component): removed tailwind class overflow-hidden
    
    ---------
    
    Co-authored-by: Iver Morland Karlsvik <iver.karlsvik@statnett.no>
    Co-authored-by: Nicolas Ovidian Winsnes Marti <nicolas@Nicolass-MacBook-Pro.local>

--- components/equipment/generic-component.tsx
+++ components/equipment/generic-component.tsx
@@ -1,63 +1,84 @@
 'use client'
-import {CIM} from "@/lib/cim";
+import { BaseVoltage, CIM } from "@/lib/cim";
 import {
-    Card,
-    CardDescription,
-    CardHeader,
-    CardTitle,
+	Card,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+	CardContent,
 } from "@/components/ui/card"
-import {Triangle} from "lucide-react";
-import {CollapsedStyling} from "../dig/flow-component";
+import { Triangle } from "lucide-react";
+import { CollapsedStyling } from "../dig/flow-component";
 import BtnGroupComponent from "../btn-group-component";
 import { colorStyling } from "../dig/flow-component";
 import { OtherData } from "@/lib/store/store-flow";
+import { ReactElement } from "react";
+import { CimPresentation } from "@/lib/cim-presentation";
+import { getTitle } from "@/lib/utils";
 
 interface ConnectivetyNodeProps {
-    equipment: CIM
-    otherData: OtherData
-    collapsed?: boolean
-    handleExpand: () => void
+	equipment: CIM
+	otherData: OtherData
+	collapsed?: boolean
+	handleExpand: () => void
+	presentation: CimPresentation
 }
 
+export default function GenericComponent({ equipment, otherData, collapsed, handleExpand, presentation }: ConnectivetyNodeProps) {
+	const title = getTitle(equipment)
+	const { size, icon } = presentation
+	const truncateClass = "truncate-text"
 
-export default function GenericComponent({equipment, otherData, collapsed, handleExpand}: ConnectivetyNodeProps) {
+	if (collapsed)
+		return (
+			<>
+				{colorStyling(otherData.color ?? "black")}
+				<div className={`${CollapsedStyling()} flex items-center`}>
+					<div className="shrink-0">
+						{icon}
+					</div>
+					<div className={`text-m ml-2 ${truncateClass}`}>{equipment.name as string}</div>
+				</div>
+			</>
+		)
 
-    if (collapsed)
-        return (
-            <>
-                {colorStyling(otherData.color ?? "black")}
-                <div className={`${CollapsedStyling()} flex items-center`}>
-                    <Triangle className="w-10 h-10"/>
-                    <div className="overflow-hidden text-m ml-2">{equipment.name as string}</div>
-                </div>
-            </>
-        )
+	return (
+		<div>
+			<BtnGroupComponent equipment={equipment} handleExpand={handleExpand} />
+			<Card className={size()} color={otherData.color ?? "black"}>
+				<CardHeader>
+					<CardTitle className="flex min-w-0 gap-2">
+						<div className="shrink-0">
+							{icon}
+						</div>
+						<div className={`${truncateClass} text-sm font-medium`}>
+							{title}
+						</div>
+					</CardTitle>
+					<CardDescription>
+						<>
+							{equipment.name &&
+								<div className={`${truncateClass} text-xs text-gray-400`}
+									title={equipment.name as string}>{equipment.name as string}
+								</div>}
+						</>
+					</CardDescription>
+				</CardHeader>
+				{equipment.description && size.name !== "smallComponentStyling" &&
+					<CardContent className="flex flex-col text-gray-600">
+						<div className={`text-gray-400 ${truncateClass}`}>{equipment.description.toString()}</div>
+						{equipment.baseVoltage && (
+							<span className={`${truncateClass}`}> Voltage {(equipment.baseVoltage as BaseVoltage).name}</span>
+						)}
+						{equipment.maxOperatingP && (
+							<span className={`${truncateClass}`}> Max operating power limit {equipment.maxOperatingP.toString()}</span>
+						)}
 
-    return (
-        <div>
-            <BtnGroupComponent equipment={equipment} handleExpand={handleExpand}/>
-            <Card className="w-[250px]" color={otherData.color ?? "black"}>
-                <CardHeader>
-                    <CardTitle className="flex justify-between">
-                        <div className="flex flex-row items-center gap-2">
-                            <Triangle/>
-                            <div className="w-40 truncate overflow-hidden text-ellipsis text-xs text-gray-400"
-                                 title={equipment.rdfType as string}>{equipment.rdfType}
-                            </div>
-                        </div>
-                    </CardTitle>
-                    <CardDescription>
-                        <>
-                            {equipment.name &&
-                                <div className="w-40 truncate overflow-hidden text-ellipsis text-xs text-gray-400"
-                                     title={equipment.name as string}>{equipment.name as string}
-                                </div>}
-                        </>
-                    </CardDescription>
-                </CardHeader>
-            </Card>
-        </div>
-    )
+					</CardContent>
+				}
+			</Card>
+		</div>
+	)
 }
 
 
